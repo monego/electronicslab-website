fb4cb273adacc295bf04d72d5643b28a216b6e42
Vinicius Monego <monego@posteo.net>
2024-07-05 12:37:06 -0300

    backend: Add static files.

--- backend/nupedee/settings.py
+++ backend/nupedee/settings.py
@@ -163,10 +163,6 @@ USE_I18N = True
 
 USE_TZ = True
 
-LOGIN_REDIRECT_URL = 'http://localhost:9000/#/'
-LOGIN_URL = 'login'
-LOGOUT_URL = 'logout'
-
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
@@ -174,6 +170,8 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILE_DIRS = [os.path.join(BASE_DIR, 'static'), ]
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
