47db629967270824ceecdffbfb248e3d2f94428f
Vinicius Monego <monego@posteo.net>
2024-07-05 14:36:04 -0300

    backend: Decouple ALLOWED_HOSTS and DEBUG.

--- backend/nupedee/settings.py
+++ backend/nupedee/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', cast=bool)
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=lambda v: [s.strip() for s in v.split(',')])
 
 
 # Application definition
