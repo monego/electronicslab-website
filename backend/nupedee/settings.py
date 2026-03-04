9effceaa354f5f32c456bebcf754d357dc1b11af
Vinicius Monego <monego@posteo.net>
2026-03-04 09:07:48 -0300

    backend: Increase 'anon' throttle rate to 1000.

--- backend/nupedee/settings.py
+++ backend/nupedee/settings.py
@@ -87,7 +87,7 @@ REST_FRAMEWORK = {
         'rest_framework.throttling.UserRateThrottle'
     ],
     'DEFAULT_THROTTLE_RATES': {
-        'anon': '100/day',
+        'anon': '1000/day',
         'user': '1000/day'
     }
 }
